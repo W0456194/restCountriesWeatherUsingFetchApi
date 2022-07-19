28d29bedba7b9de7b71b66ca6261b7ac7d57659f
W0456194 <82927744+W0456194@users.noreply.github.com>
2022-07-19 14:49:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// API for get requests
+let fetchRes = fetch(
+    "https://restcountries.com/v2/{service}?fields={name},{capitol},{Latlng},{Flag},{countryCode},{region},{subregion},{population},{area},{timezones},{currencies},{languages},{borders}}");
+//https://restcountries.com/v2/all?fields=name,capital,currencies
+            // fetchRes is the promise to resolve
+            // it by using.then() method
+            fetchRes.then(res =>
+                res.json()).then(d => {
+                    console.log(d)
+                })
+
